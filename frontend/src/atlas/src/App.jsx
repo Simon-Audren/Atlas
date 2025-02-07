99024b358788c8b21ce1e643405014350d92b486
Simon Audren <saudren@ensc.fr>
2025-02-07 12:22:45 +0100

    Début du front

---
+++ frontend/src/atlas/src/App.jsx
@@ -0,0 +1,28 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <a>
+          
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Bienvenue sur Atlas !</h1>
+    </>
+  )
+}
+
+export default App
